0a1b1b5c26d4f670008c85ef0bdf4807be27f71a
Mykhailenko <juliam4433@gmail.com>
2022-09-09 15:17:30 +0200

    update

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import css from './FriendList.module.css';
+import { FriendListItem } from "../FriendListItem/FriedListItem"; 
+
+export const FriendList = ({ friends }) => {
+    // console.log(friends);
+    return (
+        <ul className={css.friendList}>
+            {friends.map(({ id, isOnline, avatar, name }) => (
+            <FriendListItem
+            key={id}
+          isOnline={isOnline}
+          avatar={avatar}
+          name={name}
+            />
+            )         
+            )}
+        </ul>
+    );
+};
+
+FriendList.prototype = {
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      isOnline: PropTypes.bool,
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+    })
+  ).isRequired,
+};
---
+++ src/components/FriendListItem/FriedListItem.jsx
@@ -0,0 +1,21 @@
+import { type } from "@testing-library/user-event/dist/type";
+import PropTypes from "prop-types";
+import css from './FriendListItem.module.css';
+
+
+
+export const FriendListItem = ({avatar, name, isOnline }) => {
+    return (
+<li className={css.item}>
+  <span className={isOnline ? css.status__true : css.status__false}></span>
+  <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+            <p className={css.name}>{name}</p>
+</li>
+    );
+};
+
+FriendListItem.prototype = {
+  isOnline: PropTypes.bool.isRequired,
+  avatar: PropTypes.string.isRequired, 
+  name: PropTypes.string.isRequired,
+};
